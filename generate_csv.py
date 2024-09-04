7849ddac2ae691c1c9830d28a6683fe8b30f8ef4
Aaron <aaron.sl.chow@gmail.com>
2024-09-04 15:43:52 -0400

    Added Input Check

--- generate_csv.py
+++ generate_csv.py
@@ -1,10 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 import csv
 import random
 
 def generate_data(num_rows, num_cols):
     return [[random.randint(0, 10) for _ in range(num_cols)] for _ in range(num_rows)]
+
+def check_inputs(input1, input2):
+    try:
+        # Get the input values from the entries
+        value1 = int(input1.get())
+        value2 = int(input2.get())
+        
+        return True
+        
+    except ValueError:
+        messagebox.showwarning("Invalid Input", "Please enter valid numbers in both fields.")
+        # If a ValueError occurs, show a warning message
+        return False
 
 def generate_csv(data):
     file_path = filedialog.asksaveasfilename(
@@ -21,23 +35,24 @@ def generate_csv(data):
         print(f"File saved as {file_path}")
 
 def on_button_click(num_rows, num_cols):
-    data = generate_data(num_rows, num_cols)
-    generate_csv(data)
+    if check_inputs(num_rows, num_cols):
+        data = generate_data(int(num_rows.get()), int(num_cols.get()))
+        generate_csv(data)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry('200x200')
+    root.geometry('200x300')
     root.title("CSV generator")
 
     tk.Label(root, text="Enter Number of Rows", font=('Calibri 10')).pack()
     row_str = tk.Entry(root, width=35)
-    row_str.pack()
+    row_str.pack(padx=20, pady=20)
 
     tk.Label(root, text="Enter Number of Columns", font=('Calibri 10')).pack()
     col_str = tk.Entry(root, width=35)
-    col_str.pack()
+    col_str.pack(padx=20, pady=20)
 
-    generate_button = tk.Button(root, text = "Generate CSV", command=lambda: on_button_click(int(row_str.get()),int(col_str.get())))
+    generate_button = tk.Button(root, text = "Generate CSV", command=lambda: on_button_click(row_str, col_str))
     generate_button.pack(padx=20, pady=20)
 
     root.mainloop()
